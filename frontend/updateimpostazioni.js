07ba702de475705d44cedaeec1a050b01d1efc61
HorFiend <petek.sabel@gmail.com>
2021-12-13 11:37:43 +0100

    Some move of node index

--- frontend/updateimpostazioni.js
+++ frontend/updateimpostazioni.js
@@ -1,15 +1,15 @@
 var url = "./api/impostazioni";
 var params = "animali="+document.getElementById('animali').checked;
-params += "notturna="+document.getElementById('notturna').checked;
+params += "&notturna="+document.getElementById('notturna').checked;
 if (document.getElementById('notturna').checked) {
-    params += "ore_inizio="+document.getElementById('ore_inizio').value;
-    params += "ore_fine="+document.getElementById('ore_fine').value;
+    params += "&ore_inizio="+document.getElementById('ore_inizio').value;
+    params += "&ore_fine="+document.getElementById('ore_fine').value;
 }
-params += "gps="+document.getElementById('gps').checked;
-params += "registrazione="+document.getElementById('registrazione').checked;
-// index of contatti goes from 0 to max 3
+params += "&gps="+document.getElementById('gps').checked;
+params += "&registrazione="+document.getElementById('registrazione').checked;
+// index of contatti goes from 1 to 4
 for (let i = 1; i <= 4; i++) {
-    params += "contatto"+String(i) + "="+document.getElementById('contatto'+String(i)).value;
+    params += "&contatto"+String(i) + "="+document.getElementById('contatto'+String(i)).value;
 }
 
 
